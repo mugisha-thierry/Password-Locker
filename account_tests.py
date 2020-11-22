58972edbba72800cad7b594673c4ce22c30f56d0
Mugisha Thierry <thierrymugis@gmail.com>
2020-11-22 11:00:23 +0200

    Created a test class for account

---
+++ account_tests.py
@@ -0,0 +1,28 @@
+import unittest
+from account import Account
+
+class TestAccount(unittest.TestCase):
+
+    def setUp(self):
+        self.new_account = Account("mugisha-thierry","hakiza123")
+
+    def test_init(self):
+        self.assertEqual(self.new_account.user_name,"mugisha-thierry")
+        self.assertEqual(self.new_account.password,"hakiza123") 
+
+    def test_save_account(self):
+        self.new_account.save_this_account()
+        self.assertEqual(len(Account.list_of_accounts),1)
+
+    # def test_save_multiple_accounts(self):
+    #     self.new_account.save_this_account()
+    #     second_account = Account("Ketsia-a","high123")
+    #     second_account.save_this_account()
+    #     self.assertEqual(len(Account.list_of_accounts),2)
+
+    def tearDown(second_account):
+        Account.list_of_accounts= [] 
+
+
+if __name__ == '__main__':
+    unittest.main()
