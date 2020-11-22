82cc7bf68a4bfa0ed17802e6d09b801af6a7ecf7
Mugisha Thierry <thierrymugis@gmail.com>
2020-11-22 12:10:21 +0200

    Added functions

--- account.py
+++ account.py
@@ -9,3 +9,29 @@ class Account :
 
     def save_this_account(self):
         Account.list_of_accounts.append(self)
+
+    def delete_account(self):
+
+        '''
+        delete_account method deletes a saved account from the account_list
+        '''
+
+        Account.list_of_accounts.remove(self)    
+
+
+    @classmethod
+    def check_account(cls,username, password):
+        """
+        method to check whether the account is our account_list or not
+        """
+        real_account = ""
+        for account in Account.list_of_accounts:
+            if(account.user_name == user_name and account.password == password):
+                real_account == account.user_name
+        return real_account
+
+        
+
+    @classmethod
+    def display_account(cls):
+        return cls.list_of_accounts         
